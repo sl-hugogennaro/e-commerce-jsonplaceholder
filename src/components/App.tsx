7f3cccbc3cf16c030368242a4d1715eb907da55f
skornous <hugo.gennaro@gmail.com>
2020-07-13 18:28:47 +0200

    Smaller footer + multiple cards

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import { Header } from './Header'
 import { Card } from './Card'
 
-const footerHeight = '5vh'
+const footerHeight = '15px'
 const FooterUI = styled.footer`
   height: ${footerHeight};
   position: fixed;
@@ -14,24 +14,29 @@ const FooterUI = styled.footer`
   left: 0;
   right: 0;
   font-size: 12px;
+  background: white;
 `
 const AppUI = styled.div`
   /* global styles */
 `
-
-export const App = () => (
-  <AppUI>
-    <Header />
-    <Card />
-    <FooterUI>
-      Icons made by{' '}
-      <a href="http://www.freepik.com/" title="Freepik">
-        Freepik
-      </a>{' '}
-      from{' '}
-      <a href="https://www.flaticon.com/" title="Flaticon">
-        www.flaticon.com
-      </a>
-    </FooterUI>
-  </AppUI>
-)
+const fakeData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+export const App = () => {
+  return (
+    <AppUI>
+      <Header />
+      {fakeData.map((d, i) => (
+        <Card key={i} />
+      ))}
+      <FooterUI>
+        Icons made by{' '}
+        <a href="http://www.freepik.com/" title="Freepik">
+          Freepik
+        </a>{' '}
+        from{' '}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </FooterUI>
+    </AppUI>
+  )
+}
