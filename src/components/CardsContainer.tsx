edf4ab9cccdec9942ee0f6b93519bc6091c0efab
skornous <hugo.gennaro@gmail.com>
2020-07-13 20:02:15 +0200

    Better responsive design

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -9,16 +9,22 @@ const CardsContainerUI = styled.section`
   margin: 0 auto;
   width: 100%;
   /* Mobile first, then */
-  /* Small devices (landscape phones, less than 768px) */
+  /* Small devices (landscape phones, more than 768px) */
   @media (min-width: 768.98px) {
     width: 520px;
     padding: 0px 10px;
   }
 
-  /* Large devices (desktops, less than 1200px) */
-  @media (min-width: 1200.98px) {
+  /* Medium devices (tablets, more than 1024px) */
+  @media (min-width: 768.98px) {
     width: 100%;
     display: grid;
+    grid-gap: 10px 5px;
+    grid-template-columns: auto auto;
+  }
+
+  /* Large devices (desktops, more than 1200px) */
+  @media (min-width: 1200.98px) {
     grid-gap: 15px 5px;
     grid-template-columns: auto auto auto;
   }
